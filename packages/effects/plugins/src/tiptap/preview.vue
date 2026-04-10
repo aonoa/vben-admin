5bcf60f06518409884e6e572e88928504d535fa2
aonoa <1991849113@qq.com>
2026-04-10 10:34:05 +0800

    style: 代码风格
    Signed-off-by: aonoa <1991849113@qq.com>

--- packages/effects/plugins/src/tiptap/preview.vue
+++ packages/effects/plugins/src/tiptap/preview.vue
@@ -17,7 +17,7 @@ const contentMinHeight = computed(() =>
 );
 const previewClass = computed(() =>
   cn(
-    'vben-tiptap-content bg-transparent p-0 leading-7 text-foreground',
+    'text-foreground vben-tiptap-content bg-transparent p-0 leading-7',
     props.class,
   ),
 );
